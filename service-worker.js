f229217c70110b8b76377ffe5395f5fc4ca1baab
myLogic207 <lijon.fogel@gmail.com>
2021-11-11 14:36:50 +0100

    pwa

--- service-worker.js
+++ service-worker.js
@@ -9,10 +9,6 @@ self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open(CACHE).then(function(cache) {
             console.log("Cached offline page during install");
-
-            if (offlineFallbackPage === "ToDo-replace-this-name.html") {
-                return cache.add(new Response("Update the value of the offlineFallbackPage constant in the serviceworker."));
-            }
             return cache.add(offlineFallbackPage);
         })
     );
